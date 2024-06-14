c1e23724a96178fee8961900e1794b44d45e4d30
Omkarpatil2183 <omkarpatil3821@gmail.com>
2024-06-14 21:55:44 +0530

    50% work is done

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const searchForm =document.querySelector('form');
-const movieContainer =document.querySelector('.movie_container');
+const movieContainer =document.querySelector('.movie');
 const inputBox =document.querySelector('.inputBox');
         // apikey=http://www.omdbapi.com/?i=tt3896198&apikey=4aac7fbf
         
@@ -12,28 +12,42 @@ const getmovieinfo = async (movie) =>
 
     const response = await fetch(url);
     const data = await response.json();
-    // console.log(data)
+    console.log(data)
      showmoviedata(data)
     }
 
 const showmoviedata = (data) =>
     {
-        const {Title,imdbRating,Genre,Language,Runtime,Actors,Released}=data; //storing data into an array
+        // const movieContainer = document.createElement('div');
+        // movieContainer.classList.add('movie-container') 
+        movieContainer.innerHTML = '';// Clear previous movie data
+        const {Title,imdbRating,Genre,Language,Runtime,Actors,Released,Plot,Poster}=data; //storing data into an array
 
         const movieElement = document.createElement('div');
-        movieElement.innerHTML=`<h2>${Title}</h2><p>${Language}</p>`;
+        movieElement.classList.add('movie-info');
+        movieElement.innerHTML=`<h2>${Title}</h2><p><strong>Rating : &#9733 </strong>${imdbRating}</p>`;
+
         const  movieGenreElement= document.createElement('div');
         movieGenreElement.classList.add('movie-genre');
         
         Genre.split(",").forEach(element => {
             const p=document.createElement('p');
             p.innerText=element;
-            movieGenreElement.appendChild(movieElement);
+            movieGenreElement.appendChild(p);
             
         });
+        movieElement.appendChild(movieGenreElement);
+        
+        movieElement.innerHTML +=`<p><strong>Language : </strong>${Language}</p><p><strong>Duration : </strong>${Runtime}</p><p><strong>Actors : </strong>${Actors}</p><p><strong>Released date :  </strong>${Released}</p><p><strong>Plot:  </strong>${Plot}</p>`;
 
+        // creating div for movie poster 
+        const moviePoster = document.createElement('div');
+        moviePoster.classList.add('movie-poster');
+        moviePoster.innerHTML = `<img src="${Poster}"/>`;
 
-        movieContainer.innerHTML = ''; // Clear previous movie data
+        movieContainer.appendChild(moviePoster);
+        movieContainer.appendChild(movieElement);
+
         movieContainer.appendChild(movieElement);
     }
 
@@ -48,4 +62,4 @@ if (movieName !== " ")
         getmovieinfo(movieName);
     }        
 })
-console.log("Hello")
+// console.log("Hello")
